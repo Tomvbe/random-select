05af9962a61300908cbd57a503ff8585e2077b61
Tomvbe <34196062+Tomvbe@users.noreply.github.com>
2023-03-28 22:50:22 +0200

    Feat/create basic functionality (#1)
    * Init
    
    * Basic impl
    
    * add shortcuts
    
    * add todos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Renderer2, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'random-select';
+
+  fields = [{id: 'input'+0, value: ""}];
+  result!: string;
+
+  seq = 0;
+
+  constructor(private renderer: Renderer2) {}
+
+  add() {
+    const newSeq = ++this.seq;
+    this.fields.push({id: 'input'+newSeq, value: ""});
+    setTimeout(() => (document.querySelector('#input'+newSeq) as any).focus(), 0);
+  }
+
+  submit() {
+    const index = Math.floor(Math.random() * this.fields.length);
+    this.result = this.fields[index].value;
+  }
+
 }
